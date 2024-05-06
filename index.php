52516264cce1e2bda60dc71dbde486ffa4d305e5
Ephraim Bokuma Mulenga <ephraim.bokuma-mulenga@epitech.eu>
2024-05-06 16:24:26 +0200

    [ADD] Added index.php file

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * The site's entry point.
+ *
+ * Loads the relevant template part,
+ * the loop is executed (when needed) by the relevant template part.
+ *
+ * @package Church Theme
+ */
+
+get_header();
+
+get_footer();
